51072f2ae3cef74628a7696d0304471f3896a07f
cahcharm <126590861+CahCharm@users.noreply.github.com>
2025-08-01 11:27:34 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -1,17 +1,33 @@
-# Entrada do nome e da idade (funções built-in)
-nome = input("Digite seu nome: ")
-idade = int(input("Digite sua idade: "))
 
-# Estrutura condicional para verificar se pode votar
-if idade < 16:
-    situacao = "Você ainda não pode votar."
-elif idade >= 16 and idade < 18:
-    situacao = "Voto opcional (facultativo)."
-elif idade >= 18 and idade <= 70:
-    situacao = "Voto obrigatório."
-else:
-    situacao = "Voto opcional por idade."
+nome = input("Digite seu nome: ") #digita valor, "pergunta"
 
-# Exibindo o resultado 
-print(f"\nOlá, {nome}!")
-print(f"Com {idade} anos: {situacao}")
+idade = None 
+
+while True:  # laço que continua até que a condição de parar funcione
+    try: #tenta executar o que esta dentro dele 
+        idade = int(input("Digite sua idade: "))  
+        if idade < 0:  # verifica se a idade é negativa, tbm válidas
+            print("Por favor, digite uma idade válida.") 
+        else:
+            break  # sai do loop se a idade for válida
+    except ValueError:  #captura exceções (except), caso try nn der certo
+        print("Por favor, digite um número inteiro.") 
+
+faixas = [
+    (16, "Você ainda não pode votar."), 
+    (18, "Voto opcional (facultativo)."),  
+    (70, "Voto obrigatório.")
+]
+
+
+situacao = ""  # variável que armazena a mensagem correspondente à situação de voto
+
+
+for limite, mensagem in faixas:  # laço que percorre cada faixa etária 
+    if idade < limite:  # verifica se a idade é menor que o limite
+        situacao = mensagem  # atribui a mensagem correspondente
+        break  # sai do laço 
+
+
+print(f"\nOlá, {nome}!") 
+print(f"Com {idade} anos: {situacao}") 
